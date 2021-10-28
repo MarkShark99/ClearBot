e5764dfbc6d646efabe609e2f7280ab5435f5dfd
Mark Washington <markdw88@gmail.com>
2021-10-28 00:41:09 -0400

    Documentation

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import * as dotenv from "dotenv";
 // Read in configuration from .env file
 dotenv.config();
 
+// Verify that environment variables exist
 if (!("token" in process.env)) throw new Error("\"token\" not found in .env file");
 if (!("clientId" in process.env)) throw new Error("\"clientId\" not found in .env file");
 if (!("guildId" in process.env)) throw new Error("\"guildId\" not found in .env file");
@@ -19,30 +20,42 @@ const token = process.env.token as string;
 const clientId = process.env.clientId as string;
 const guildId = process.env.guildId as string;
 
+// Create bot client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Wait for client to be ready
 client.once('ready', () => console.log("Bot is ready!"));
 
+// Run this when bot is live and added to a server to automatically add commands
 client.on("guildCreate", () => {
   console.log("Bot has joined a new server!");
+  
+  // Create list of commands that we want to be able to use
   const commands = [
-    new SlashCommandBuilder().setName("ping").setDescription("Replies with pong"),
     new SlashCommandBuilder().setName("clear").setDescription("Clear!"),
   ].map(command => command.toJSON());
 
+  // Create REST object
   const rest = new REST({ version: '9' }).setToken(token);
 
+  // Submit slash commands
   rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
   .then(() => console.log("Successfully registered commands in server"))
   .catch(console.error);
 });
 
+// Run this when someone interacts with the bot
 client.on('interactionCreate', async (interaction) => {
+  // Verify that this is a command
   if (!interaction.isCommand()) return;
 
+  // Extract command name and verify that it equals 'clear'
   const commandName = interaction.commandName;
   if (commandName === "clear" ) {
+    // Create db client
     const client = new DynamoDBClient({ region: "us-east-2" });
+    
+    // Create command to increment value for Key 'count' in Clear-Button table
     const dbUpdateCommand = new UpdateItemCommand({
       TableName: "Clear-Button",
       Key: {
@@ -54,19 +67,18 @@ client.on('interactionCreate', async (interaction) => {
       ExpressionAttributeValues: { ":one": { N: "1" } },
       ReturnValues: "UPDATED_NEW"
     });
-  
+    
     let count;
+
     const response = client.send(dbUpdateCommand);
     await response.then(async (value) => {
+      // If everything worked with DynamoDB, reply with the next value
       if (value.Attributes && value.Attributes.V.N) {
-        count = value.Attributes.V.N;
+        await interaction.reply(`Clear! (${value.Attributes.V.N})`);
       } else {
         await interaction.reply("Unclear...");
-        return;
       }
     }).catch(console.error);
-    
-    await interaction.reply(`Clear! (${count})`);
   }
 });
 
